3de06e192a7e8f2a5281b214a2a749dbc3a5eb92
OETS <socialoetsune@gmail.com>
2017-06-17 19:33:04 +0300

    Create MerhabaDunya.java

---
+++ MerhabaDunya/MerhabaDunya.java
@@ -0,0 +1,9 @@
+/**
+ * Created by OETS on 17.06.2017.
+ */
+ 
+public class MerhabaDunya {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dünya!"); //Ekrana yazılacak olan metni belirler. Ardından imleç bir alt satıra geçer.(println)
+    }
+}
